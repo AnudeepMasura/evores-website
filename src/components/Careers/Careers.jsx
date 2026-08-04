0789156c5eb3095b587c353e367d5f37faf9d320
Pragnasriyerra <237r1a66c7@cmrtc.ac.in>
2026-08-04 16:23:36 +0530

    Update Careers.jsx

--- src/components/Careers/Careers.jsx
+++ src/components/Careers/Careers.jsx
@@ -1,41 +1,30 @@
 import "./Careers.css";
 
-function Careers(){
+function Careers() {
+  return (
+    <section id="careers">
+      <div className="container">
 
-return(
+        <h2 className="section-title">
+          Our <span>Careers.</span>
+        </h2>
 
-<section id="careers">
+        <div className="text-block">
+          <p>
+            We are constantly seeking brilliant minds to join our corporate journey.
+          </p>
 
-<div className="container">
+          <a
+            href="#"
+            className="career-btn"
+          >
+            Go to Careers
+          </a>
+        </div>
 
-<h2 className="section-title">
-
-Our <span>Careers.</span>
-
-</h2>
-
-<div className="text-block">
-
-<p>
-
-At EvoRES Technology, we believe in evolving research and implementing highly reliable engineering designs. We are constantly seeking brilliant minds to join our corporate journey.
-
-</p>
-
-<p>
-
-Strategic recruitment metrics and active opening pipelines will be posted soon. Stay tuned for career opportunities with us.
-
-</p>
-
-</div>
-
-</div>
-
-</section>
-
-)
-
+      </div>
+    </section>
+  );
 }
 
 export default Careers;
